8f7ee6824cff70a5efc6d260164506be41143a45
Will Urmston <wurmston@risd.edu>
2017-07-27 00:44:43 -0700

    lib

---
+++ squish.js
@@ -0,0 +1,137 @@
+
+
+;(function () {
+  'use strict'
+  // GLOBAL VARIABLES
+  var windowW = getWindowWidth()
+  var windowH = getWindowHeight()
+
+  var scaleString = ' scale(1,1)'
+
+  // UTILITIES
+  function getWindowWidth() {
+    var w = window,
+        d = document,
+        e = d.documentElement,
+        g = d.getElementsByTagName('body')[0],
+        x = w.innerWidth || e.clientWidth || g.clientWidth
+
+    return x
+  }
+
+  function getWindowHeight() {
+    var w = window,
+        d = document,
+        e = d.documentElement,
+        g = d.getElementsByTagName('body')[0],
+        y = w.innerHeight || e.clientHeight || g.clientHeight
+
+    return y
+  }
+
+  function remap(x, in_min, in_max, out_min, out_max) {
+    if (x < in_min) {
+      x = in_min
+    }
+    if (x > in_max) {
+      x = in_max
+    }
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+  }
+
+
+
+  // CREATE CONSTRUCTOR
+  function Squish(element) {
+    if (typeof element === 'string') {
+      element = document.querySelector(element)
+    }
+
+    this.element = element || document.body
+    this.element.style.transform = scaleString
+    var _this = this
+    window.addEventListener('resize', function() {
+      resizeHandler(_this)
+    }, false)
+  }
+
+  Object.assign(Squish.prototype, {
+    scaleW: 1,
+    scaleH: 1,
+    isFrozen: false
+  })
+
+  // called on every resize
+  function resizeHandler(squishInstance) {
+    windowW = getWindowWidth()
+    windowH = getWindowHeight()
+
+    if (squishInstance.isFrozen) {
+
+      // calculate scaleW
+      squishInstance.scaleW =  windowW / squishInstance.frozenW
+      // calculate scaleH
+      squishInstance.scaleH = windowH / squishInstance.frozenH
+
+      squishInstance.updateCSS()
+
+
+    } else {
+
+      squishInstance.scaleW = 1
+      squishInstance.scaleH = 1
+    }
+
+  }
+
+  Squish.prototype.updateCSS = function() {
+    this.element.style.width = this.frozenW+'px'
+    this.element.style.width = this.frozenH+'px'
+    this.element.style['transform-origin'] = 'top left'
+    var transformString = this.element.style.transform
+    var newScaleString = ' scale('+this.scaleW+','+this.scaleH+')'
+    // this.element.style.transform = transformString.replace(scaleString, newScaleString)
+    this.element.style.transform = newScaleString
+    scaleString = newScaleString
+  }
+
+  Squish.prototype.clearCSS = function() {
+    var transformString = this.element.style.transform
+    // do better parsing of transforms here, it shouldnt overwrite other transforms
+    transformString = ''
+    this.element.style.transform = transformString
+  }
+
+  // API
+  Squish.prototype.freeze = function(width, height) {
+    this.isFrozen = true
+
+    if (width && height) {
+      this.frozenW = width
+      this.frozenH = height
+    } else {
+      this.frozenW = windowW
+      this.frozenH = windowH
+    }
+
+    this.updateCSS
+
+    return this
+  }
+
+  Squish.prototype.unfreeze = function() {
+    if (this.isFrozen) {
+      this.isFrozen = false
+      document.body.transform = ''
+
+      this.clearCSS()
+    }
+
+    return this
+  }
+
+  // export constructor
+  window.Squish = Squish
+
+
+})();
